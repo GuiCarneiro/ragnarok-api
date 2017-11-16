17967164fcc847fde4323bf732ebac57278c8aa1
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-11-16 10:45:39 -0300

    improving organization

--- app.rb
+++ app.rb
@@ -1,28 +1,10 @@
 require 'rubygems'
 require 'bundler'
-require 'sinatra'
-require 'bundler'
-require 'nokogiri'
-require 'sinatra/reloader' if development?
-require 'sinatra/json'
-require './controller'
-require './model'
-require './helpers'
+
+# Irá requerer todos as nossas dependencias
+# por meio do bundler
 Bundler.require(:default)
 
-configure do
-	enable :sessions
-	set :json_encoder, :to_json
-	set :erb, :layout => :layout
-end
-
-before do
-  headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-  headers['Access-Control-Allow-Origin'] = '*'
-  headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
-end
-
-options '*' do
-  response.headers['Allow'] = 'HEAD,GET,PUT,DELETE,OPTIONS,POST'
-  response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
-end
+# Irá inicializar qualquer arquivo .rb
+# que encontrar dentro das pastas
+Dir["./*/*.rb"].each {|file| require file }
--- config.ru
+++ config.ru
@@ -1,4 +1,17 @@
 require 'sinatra'
 require './app'
 
+# Setando o nosso json encoder
+configure do
+  set :json_encoder, :to_json
+end
+
+# Configurando os headers das nossas requests
+before do
+  headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
+end
+
+#Iniciando nosso App
 run Sinatra::Application
